f3f199b6ec1cb5691905a386d13c694021099843
nonam00 <raiskt.ilya3@yandex.ru>
2024-02-23 22:57:58 +0300

    removed gitignore files

--- Backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ Backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36508a654c3b3cab8fb11f09197d8566c07a39c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ea8141c2f749acd25e1f34bf7a1268af24142f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
